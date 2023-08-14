7dd1ac940799411d55c1107f08615c6c82113b62
Abraham <kurerelagat01@gmail.com>
2023-08-14 11:57:48 +0300

    PHP init

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+        $incomingFields = $request -> validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        return 'hello from our controller';
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+ 
+  <div style="border: 3px solid black;">
+  <h1>Register</h1>
+    <form action="/register" method="POST">
+      @csrf
+      <input name="name" type="text" placeholder="name">
+      <input name="email" type="text" placeholder="email">
+      <input name="password" type="password" placeholder="password">
+      <button>Register</button>
+    </form>
+  </div>
+</body>
+</html>
